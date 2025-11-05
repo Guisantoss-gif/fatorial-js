34c86e6dfc6abe02101428f00c7d5c39620032bd
Guisantoss-gif <go9104390@gmail.com>
2025-11-04 23:58:26 -0300

    Add files via upload

---
+++ fatorial.js
@@ -0,0 +1,13 @@
+function fatorial(n) {
+  if (n < 0) {
+    throw new Error("O número deve ser não negativo.");
+  }
+
+  let resultado = 1;
+  for (let i = 2; i <= n; i++) {
+    resultado *= i;
+  }
+  return resultado;
+}
+
+module.exports = fatorial;
